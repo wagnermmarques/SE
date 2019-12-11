f62539d96115b67aa5c50c7f27e4171e2f5d155d
wagnermmarques <54187998+wagnermmarques@users.noreply.github.com>
2019-12-11 11:15:05 -0300

    Teste de tx/rx nrf24l01

---
+++ trabalhofinal/mundo_Test.ino
@@ -0,0 +1,27 @@
+    #include <SPI.h>
+    #include <nRF24L01.h>
+    #include <RF24.h>
+    RF24 radio(7, 8); // CE, CSN
+    const byte address[6] = "00001";
+    void setup() {
+      Serial.begin(9600);
+      radio.begin();
+      radio.openWritingPipe(address);
+      radio.openReadingPipe(0, address);
+      radio.startListening();
+      radio.setPALevel(RF24_PA_MIN);
+      }
+    void loop() {
+      radio.stopListening();
+      const char text[] = "Hello Mundo";
+      radio.write(&text, sizeof(text));
+      radio.startListening(); 
+      delay(100);
+      
+      
+      if (radio.available()) {
+      char text[32] = "";
+      radio.read(&text, sizeof(text));
+      Serial.println(text);
+      }
+    }
